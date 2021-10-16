219b53cf7e375b4cca5577b1711e24ed0d28ba07
aida-solat <aidasolatco@gmail.com>
2021-10-17 00:22:32 +0300

    Updates

---
+++ precache-manifest.c70c822a515e62818ea4d6faf457f5f1.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cef1fb0b7e9b8b7ba4a8540d6016615",
+    "url": "/Project-Card/index.html"
+  },
+  {
+    "revision": "735a4c9a811b73cf048e",
+    "url": "/Project-Card/static/js/2.b9adc3d7.chunk.js"
+  },
+  {
+    "revision": "bc09e758455b40bcd61c",
+    "url": "/Project-Card/static/js/main.7949d900.chunk.js"
+  },
+  {
+    "revision": "b5a246a03e0175f7d4ca",
+    "url": "/Project-Card/static/js/runtime~main.e32fa535.js"
+  }
+]);
